44e1fc9004ef8fb0b585d6ad835ab8e67bf64748
Mai Ngo <mai@rocketgate.com>
2021-09-14 12:42:32 -0400

    RGOPS-874 Updated PHP SDK Version

--- src/GatewayChecksum.php
+++ src/GatewayChecksum.php
@@ -11,8 +11,8 @@ namespace RocketGate\Sdk;
 class GatewayChecksum
 {
     public static $checksum = "";
-    public static $baseChecksum = "cdf5b37adb9eaa4e15508263b8966610";
-    public static $versionNo = "P7.16";
+    public static $baseChecksum = "133cbfbf9de1c8b554a567cf834ce3d1";
+    public static $versionNo = "P7.17";
 
 //////////////////////////////////////////////////////////////////////
 //
@@ -30,7 +30,7 @@ class GatewayChecksum
             md5_file($dirName . "/GatewayCodes.php");
         GatewayChecksum::$checksum = md5($baseString);
         if (GatewayChecksum::$checksum != GatewayChecksum::$baseChecksum) {
-            GatewayChecksum::$versionNo = "P7.16m";
+            GatewayChecksum::$versionNo = "P7.17m";
         }
     }
 }
