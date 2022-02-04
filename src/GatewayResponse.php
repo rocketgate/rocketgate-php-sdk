eaeeb571928b68bc3849141f2782d924fd3d2bd8
Nick Sansone <nick@rocketgate.com>
2022-02-04 15:36:01 -0500

    Update GatewayResponse.php

--- src/GatewayResponse.php
+++ src/GatewayResponse.php
@@ -470,8 +470,57 @@ class GatewayResponse extends GatewayParameterList
     {
         return "schemeSettlementDate";
     }
+
 	static function PAYMENT_LINK_URL() 
 	{ 
 		return "PAYMENT_LINK_URL"; 
 	}
+
+    static function _3DSECURE_DS_TRANSACTION_ID() {
+        return "_3DSECURE_DS_TRANSACTION_ID";
+    }
+
+    static function _3DSECURE_PARESSTATUS() {
+        return "_3DSECURE_PARESSTATUS";
+    }
+
+    static function _3DSECURE_CAVV_UCAF() {
+        return "_3DSECURE_CAVV_UCAF";
+    }
+
+    static function _3DSECURE_CAVV_ALGORITHM() {
+        return "_3DSECURE_CAVV_ALGORITHM";
+    }
+
+    static function _3DSECURE_LOOKUP_SIGNATURE() {
+        return "_3DSECURE_LOOKUP_SIGNATURE";
+    }
+
+    static function _3DSECURE_XID() {
+        return "_3DSECURE_XID";
+    }
+
+    static function _3DSECURE_ACS_TRANSACTION_ID() {
+        return "_3DSECURE_ACS_TRANSACTION_ID";
+    }
+
+    static function _3DSECURE_THREE_DS_SERVER_TRANSACTION_ID() {
+        return "_3DSECURE_THREE_DS_SERVER_TRANSACTION_ID";
+    }
+
+    static function _3DSECURE_LOOKUP_CHALLENGE_INDICATOR() {
+        return "_3DSECURE_LOOKUP_CHALLENGE_INDICATOR";
+    }
+
+    static function _3DSECURE_CHALLENGE_MANDATED_INDICATOR() {
+        return "_3DSECURE_CHALLENGE_MANDATED_INDICATOR";
+    }
+
+    static function _3DSECURE_VERSTATUS() {
+        return "_3DSECURE_VERSTATUS";
+    }
+
+    static function _3DSECURE_LOOKUP_REFERENCE_GUID() {
+        return "_3D_LOOKUP_REFERENCE_GUID";
+    }
 }
