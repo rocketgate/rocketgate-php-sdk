858ccef136ee6993e0dd312e46d8bbfa10c8040c
petricarg <96256020+petricarg@users.noreply.github.com>
2023-04-27 18:19:52 +0300

    Add Google Pay Token gateway request parameter. Release PHP SDK P8.4 version. (#56)
    * Add Google Pay Token gateway request parameter.
    
    * Update version P8.4
    
    ---------
    
    Co-authored-by: Petrica RG <petrica@localhost>

--- src/GatewayChecksum.php
+++ src/GatewayChecksum.php
@@ -11,8 +11,8 @@ namespace RocketGate\Sdk;
 class GatewayChecksum
 {
     public static $checksum = "";
-    public static $baseChecksum = "9502c24171fc4a5c36a004b8be7c8023";
-    public static $versionNo = "P8.3";
+    public static $baseChecksum = "29482d8bca93b13efe31de053c57b22a";
+    public static $versionNo = "P8.4";
 
 //////////////////////////////////////////////////////////////////////
 //
@@ -30,7 +30,7 @@ class GatewayChecksum
             md5_file($dirName . "/GatewayCodes.php");
         GatewayChecksum::$checksum = md5($baseString);
         if (GatewayChecksum::$checksum != GatewayChecksum::$baseChecksum) {
-            GatewayChecksum::$versionNo = "P8.3m";
+            GatewayChecksum::$versionNo = "P8.4m";
         }
     }
 }
