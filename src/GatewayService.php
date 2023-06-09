30fc2b9e87009c766da59ccf9565a775875ecab7
Lucas C <lucas@rocketgate.com>
2023-06-09 14:09:12 -0400

    RGOPS-4421: GatewayService, make the Port field optional from Gateway URL for targeted transactions

--- src/GatewayService.php
+++ src/GatewayService.php
@@ -563,7 +563,9 @@ class GatewayService
                 $request->Set("gatewayServer", $urlBits['host']);
             }
             $request->Set("gatewayProtocol", $urlBits['scheme']);
-            $request->Set("gatewayPortNo", $urlBits['port']);
+            if (array_key_exists("port", $urlBits)) {
+                $request->Set("gatewayPortNo", $urlBits['port']);
+            }
             $request->Set("gatewayServlet", $urlBits['path'] . "?" . $urlBits['query']);
         }
 
