7aaade9c29aae77797d1965b808293f5e88d11b9
Nick-RG <71093873+Nick-RG@users.noreply.github.com>
2023-06-22 11:39:41 -0400

    RGOPS-4415 (#58)
    Introduce a new Gateway parameter in the GatewayRequest so merchants can control the window size.

--- src/GatewayChecksum.php
+++ src/GatewayChecksum.php
@@ -11,8 +11,8 @@ namespace RocketGate\Sdk;
 class GatewayChecksum
 {
     public static $checksum = "";
-    public static $baseChecksum = "29482d8bca93b13efe31de053c57b22a";
-    public static $versionNo = "P8.4";
+    public static $baseChecksum = "ec9b61519a1cd328d3da46940c29aab3";
+    public static $versionNo = "P8.5";
 
 //////////////////////////////////////////////////////////////////////
 //
@@ -30,7 +30,7 @@ class GatewayChecksum
             md5_file($dirName . "/GatewayCodes.php");
         GatewayChecksum::$checksum = md5($baseString);
         if (GatewayChecksum::$checksum != GatewayChecksum::$baseChecksum) {
-            GatewayChecksum::$versionNo = "P8.4m";
+            GatewayChecksum::$versionNo = "P8.5m";
         }
     }
 }
--- src/GatewayRequest.php
+++ src/GatewayRequest.php
@@ -576,6 +576,22 @@ class GatewayRequest extends GatewayParameterList
 	{
 		return "_3DSECURE_CHALLENGE_MANDATED_INDICATOR";
 	}
+
+    /**
+     * An override field that a merchant can pass in to set the challenge window size to display to the end cardholder.
+     * The ACS will reply with content that is formatted appropriately to this window size to allow for the best user experience.
+     * The sizes are width x height in pixels of the window displayed in the cardholder browser window.
+     * The possible values are:
+     * 01 (250x400)
+     * 02 (390x400)
+     * 03 (500x600)
+     * 04 (600x400)
+     * 05 (Full page)
+     */
+    static function _3DSECURE_ACS_WINDOW_SIZE()
+    {
+        return "_3DSECURE_ACS_WINDOW_SIZE";
+    }
 	
     static function _3DSECURE_THREE_DS_SERVER_TRANSACTION_ID() 
     { 
