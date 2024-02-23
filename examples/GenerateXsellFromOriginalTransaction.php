cdcd721fd47669578941966f6a75fbaf86f4949f
akintunde-rocketgate <akintunde@rocketgate.com>
2024-02-23 07:05:50 +0000

    RGOPS-5175: Added invoiceId lookup to the example

--- examples/GenerateXsellFromOriginalTransaction.php
+++ examples/GenerateXsellFromOriginalTransaction.php
@@ -47,8 +47,9 @@ $request->Set(GatewayRequest::MERCHANT_PASSWORD(), "testpassword");
 // For example/testing, we set the order id and customer as the unix timestamp as a convienent sequencing value
 // appending a test name to the order id to facilitate some clarity when reviewing the tests
 $time = time();
+$inv_id = $time . '.GenerateXSellFromOrig';
 $request->Set(GatewayRequest::MERCHANT_CUSTOMER_ID(), $time . '.PHPTest');
-$request->Set(GatewayRequest::MERCHANT_INVOICE_ID(), $time . '.GenerateXSellFromOrig');
+$request->Set(GatewayRequest::MERCHANT_INVOICE_ID(), $inv_id);
 
 $request->Set(GatewayRequest::AMOUNT(), "9.99");
 $request->Set(GatewayRequest::CURRENCY(), "USD");
@@ -80,7 +81,7 @@ $request->Set(GatewayRequest::AVS_CHECK(), "IGNORE");
 $service->SetTestMode(TRUE);
 
 //
-//	Perform an Auth-Only transaction.
+//	Step 1: Perform an Auth-Only transaction.
 //
 if ($service->PerformAuthOnly($request, $response)) {
     print "Original transaction succeeded\n";
@@ -89,74 +90,102 @@ if ($service->PerformAuthOnly($request, $response)) {
     print "Scheme TransactionId: " .  $response->Get(GatewayResponse::SCHEME_TRANSACTION_ID()) . "\n";
     print "Scheme SettlementDate: " .  $response->Get(GatewayResponse::SCHEME_SETTLEMENT_DATE()) . "\n";
 
-    //Create a new request to use the scheme transactionId and settlementDate of the original transaction to create a Xsell transaction
+    // Compose a request to lookup the invoiceId
     $request = new GatewayRequest();
-    $service = new GatewayService();
-
     $request->Set(GatewayRequest::MERCHANT_ID(), "1");
     $request->Set(GatewayRequest::MERCHANT_PASSWORD(), "testpassword");
 
-    // For example/testing, we set the order id and customer as the unix timestamp as a convienent sequencing value
-    // appending a test name to the order id to facilitate some clarity when reviewing the tests
-    $request->Set(GatewayRequest::MERCHANT_CUSTOMER_ID(), $time . '.PHPTest');
+    $request->Set(GatewayRequest::MERCHANT_INVOICE_ID(), $inv_id);
 
-    $time += 1;
-    $request->Set(GatewayRequest::MERCHANT_INVOICE_ID(), $time . '.GenerateXSellFromOrig');
+    // Step 2: Perform Lookup to fetch scheme transactionId and settlementDate
+    if ($service->PerformLookup($request, $response)) {
 
-    $request->Set(GatewayRequest::AMOUNT(), "3.00");
-    $request->Set(GatewayRequest::CURRENCY(), "USD");
-    $request->Set(GatewayRequest::CARD_HASH(), $response->Get(GatewayResponse::CARD_HASH()));
+        $lookupTransactionId = $response->Get(GatewayResponse::SCHEME_TRANSACTION_ID());
+        $lookupSettlementDate = $response->Get(GatewayResponse::SCHEME_SETTLEMENT_DATE());
 
-    $request->Set(GatewayRequest::CUSTOMER_FIRSTNAME(), "Joe");
-    $request->Set(GatewayRequest::CUSTOMER_LASTNAME(), "PHPTester");
-    $request->Set(GatewayRequest::EMAIL(), "phptest@fakedomain.com");
-    $request->Set(GatewayRequest::IPADDRESS(), $_SERVER['REMOTE_ADDR']);
-
-    $request->Set(GatewayRequest::BILLING_ADDRESS(), "123 Main St");
-    $request->Set(GatewayRequest::BILLING_CITY(), "Las Vegas");
-    $request->Set(GatewayRequest::BILLING_STATE(), "NV");
-    $request->Set(GatewayRequest::BILLING_ZIPCODE(), "89141");
-    $request->Set(GatewayRequest::BILLING_COUNTRY(), "US");
-
-    // Risk/Scrub Request Setting
-    $request->Set(GatewayRequest::SCRUB(), "IGNORE");
-    $request->Set(GatewayRequest::AVS_CHECK(), "IGNORE");
-
-    //Set necessary parameters required for RocketGate to port previous transaction's scheme transactionId and settlementDate
-    $request->Set(GatewayRequest::USE_PRIMARY_SCHEMEID(), "TRUE");
-    $request->Set(GatewayRequest::BILLING_TYPE(), "T");
-    $request->Set(GatewayRequest::REFERENCE_SCHEME_TRANSACTION_ID(), $response->Get(GatewayResponse::SCHEME_TRANSACTION_ID()));
-    $request->Set(GatewayRequest::REFERENCE_SCHEME_SETTLEMENT_DATE(), $response->Get(GatewayResponse::SCHEME_SETTLEMENT_DATE()));
-
-    $response = new GatewayResponse();
-
-    if ($service->PerformAuthOnly($request, $response)) {
-
-        if($request->Get(GatewayRequest::REFERENCE_SCHEME_TRANSACTION_ID()) == null || $response->Get(GatewayResponse::SCHEME_TRANSACTION_ID()) == $request->Get(GatewayRequest::REFERENCE_SCHEME_TRANSACTION_ID())){
-            $areSchemeTransactionIdSame = "true";
-        }else{
-            $areSchemeTransactionIdSame = "false";
-        }
-
-        if($request->Get(GatewayRequest::REFERENCE_SCHEME_SETTLEMENT_DATE()) == null || $response->Get(GatewayResponse::SCHEME_SETTLEMENT_DATE()) == $request->Get(GatewayRequest::REFERENCE_SCHEME_SETTLEMENT_DATE())){
-            $areSchemeSettlementDateSame = "true";
-        }else{
-            $areSchemeSettlementDateSame = "false";
-        }
-
-        print "Xsell transaction succeeded\n";
-        print "Response Code: " .  $response->Get(GatewayResponse::RESPONSE_CODE()) . "\n";
+        print "\nLookup succeeded\n";
         print "Reason Code: " .  $response->Get(GatewayResponse::REASON_CODE()) . "\n";
-        print "Request & Response Scheme transactionIds are the same? " . $areSchemeTransactionIdSame . "\n";
-        print "Request & Response Scheme settlementDates are the same? " . $areSchemeSettlementDateSame . "\n";
-    }else{
-        print "Xsell transaction failed\n";
         print "GUID: " . $response->Get(GatewayResponse::TRANSACT_ID()) . "\n";
-        print "Transaction time: " . $response->Get(GatewayResponse::TRANSACTION_TIME()) . "\n";
-        print "Response Code: " . $response->Get(GatewayResponse::RESPONSE_CODE()) . "\n";
-        print "Reason Code: " . $response->Get(GatewayResponse::REASON_CODE()) . "\n";
-        print "Exception: " . $response->Get(GatewayResponse::EXCEPTION()) . "\n";
-        print "Scrub: " . $response->Get(GatewayResponse::SCRUB_RESULTS()) . "\n";
+        print "Account: " .  $response->Get(GatewayResponse::MERCHANT_ACCOUNT()) . "\n";
+        print "Scheme TransactionId: " .  $lookupTransactionId . "\n";
+        print "Scheme SettlementDate: " .  $lookupSettlementDate . "\n";
+
+        //Create a new request to use the scheme transactionId and settlementDate of the original transaction to create a Xsell transaction
+        $request = new GatewayRequest();
+        $service = new GatewayService();
+
+        $request->Set(GatewayRequest::MERCHANT_ID(), "1");
+        $request->Set(GatewayRequest::MERCHANT_PASSWORD(), "testpassword");
+
+        // For example/testing, we set the order id and customer as the unix timestamp as a convienent sequencing value
+        // appending a test name to the order id to facilitate some clarity when reviewing the tests
+        $request->Set(GatewayRequest::MERCHANT_CUSTOMER_ID(), $time . '.PHPTest');
+
+        $time += 1;
+        $request->Set(GatewayRequest::MERCHANT_INVOICE_ID(), $time . '.GenerateXSellFromOrig');
+
+        $request->Set(GatewayRequest::AMOUNT(), "3.00");
+        $request->Set(GatewayRequest::CURRENCY(), "USD");
+        $request->Set(GatewayRequest::CARD_HASH(), $response->Get(GatewayResponse::CARD_HASH()));
+
+        $request->Set(GatewayRequest::CUSTOMER_FIRSTNAME(), "Joe");
+        $request->Set(GatewayRequest::CUSTOMER_LASTNAME(), "PHPTester");
+        $request->Set(GatewayRequest::EMAIL(), "phptest@fakedomain.com");
+        $request->Set(GatewayRequest::IPADDRESS(), $_SERVER['REMOTE_ADDR']);
+
+        $request->Set(GatewayRequest::BILLING_ADDRESS(), "123 Main St");
+        $request->Set(GatewayRequest::BILLING_CITY(), "Las Vegas");
+        $request->Set(GatewayRequest::BILLING_STATE(), "NV");
+        $request->Set(GatewayRequest::BILLING_ZIPCODE(), "89141");
+        $request->Set(GatewayRequest::BILLING_COUNTRY(), "US");
+
+        // Risk/Scrub Request Setting
+        $request->Set(GatewayRequest::SCRUB(), "IGNORE");
+        $request->Set(GatewayRequest::AVS_CHECK(), "IGNORE");
+
+        //Set necessary parameters required for RocketGate to port previous transaction's scheme transactionId and settlementDate
+        $request->Set(GatewayRequest::USE_PRIMARY_SCHEMEID(), "TRUE");
+        $request->Set(GatewayRequest::BILLING_TYPE(), "T");
+        $request->Set(GatewayRequest::REFERENCE_SCHEME_TRANSACTION_ID(), $lookupTransactionId);
+        $request->Set(GatewayRequest::REFERENCE_SCHEME_SETTLEMENT_DATE(), $lookupSettlementDate);
+
+        $response = new GatewayResponse();
+
+        //Step 3: Send a transaction to utilize the scheme transactionId and settlementDate
+        if ($service->PerformAuthOnly($request, $response)) {
+
+            if($request->Get(GatewayRequest::REFERENCE_SCHEME_TRANSACTION_ID()) == null || $response->Get(GatewayResponse::SCHEME_TRANSACTION_ID()) == $lookupTransactionId){
+                $areSchemeTransactionIdSame = "true";
+            }else{
+                $areSchemeTransactionIdSame = "false";
+            }
+
+            if($request->Get(GatewayRequest::REFERENCE_SCHEME_SETTLEMENT_DATE()) == null || $response->Get(GatewayResponse::SCHEME_SETTLEMENT_DATE()) == $lookupSettlementDate){
+                $areSchemeSettlementDateSame = "true";
+            }else{
+                $areSchemeSettlementDateSame = "false";
+            }
+
+            print "Xsell transaction succeeded\n";
+            print "Response Code: " .  $response->Get(GatewayResponse::RESPONSE_CODE()) . "\n";
+            print "Reason Code: " .  $response->Get(GatewayResponse::REASON_CODE()) . "\n";
+            print "Lookup & Xsell transaction response Scheme transactionIds are the same? " . $areSchemeTransactionIdSame . "\n";
+            print "Lookup & Xsell transaction response Scheme settlementDates are the same? " . $areSchemeSettlementDateSame . "\n";
+        }else{
+            print "Xsell transaction failed\n";
+            print "GUID: " . $response->Get(GatewayResponse::TRANSACT_ID()) . "\n";
+            print "Transaction time: " . $response->Get(GatewayResponse::TRANSACTION_TIME()) . "\n";
+            print "Response Code: " . $response->Get(GatewayResponse::RESPONSE_CODE()) . "\n";
+            print "Reason Code: " . $response->Get(GatewayResponse::REASON_CODE()) . "\n";
+            print "Exception: " . $response->Get(GatewayResponse::EXCEPTION()) . "\n";
+            print "Scrub: " . $response->Get(GatewayResponse::SCRUB_RESULTS()) . "\n";
+        }
+
+    } else {
+        print "\nLookup failed\n";
+        print "GUID: " . $response->Get(GatewayResponse::TRANSACT_ID()) . "\n";
+        print "Reason Code: " .  $response->Get(GatewayResponse::REASON_CODE()) . "\n";
+        print "Exception: " .  $response->Get(GatewayResponse::EXCEPTION()) . "\n";
     }
 
 } else {
