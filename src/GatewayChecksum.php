2374edd10405645dda8dcb9ddb40bcbdd2f67788
Mai Ngo <mai@rocketgate.com>
2024-05-16 13:47:10 -0400

    RGOPS-4858 SolidGate Brazilian CPF Support

--- src/GatewayChecksum.php
+++ src/GatewayChecksum.php
@@ -30,7 +30,6 @@ class GatewayChecksum
             md5_file($dirName . "/GatewayParameterList.php") .
             md5_file($dirName . "/GatewayCodes.php");
         GatewayChecksum::$checksum = md5($baseString);
-        print "checksum: " . GatewayChecksum::$checksum . "\n";
         if (GatewayChecksum::$checksum != GatewayChecksum::$baseChecksum) {
             GatewayChecksum::$versionNo = GatewayChecksum::VERSION . "m";
         }
